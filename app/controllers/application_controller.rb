4d61e949b2076753aaa735f7fdd7f2a57c420803
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-24 22:47:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
+  
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
-
-  get '/recipes/new' do
-    erb :new
-  end
-
+  
   get '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     erb :show
